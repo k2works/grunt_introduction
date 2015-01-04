44232e51e381f5cba221a21ab634a64fa0b7474f
k2works <kakimomokuri@gmail.com>
2015-01-04 16:06:35 +0900

    ソースコードの構文チェック

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,5 +5,11 @@ module.exports = function (grunt) {
       src: 'src',
       dist: 'dist',
     },
+    jshint: {
+      beforeconcat: ['<%= dirs.src %>/js/*.js'],
+      afterconcat: ['<%= dirs.dest %>/js/*.js']
+    }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 }
